77ca04f0d2645389742eb244d199dfe55c89299d
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-07 13:51:11 +0100

    finish

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// avviso per utente
+alert ('Qeusta pagina è in fase sperimentale, si prega di rispettare l\' inserimento dei dati così come richiesti')
+
+// recupero dati
+const userName = prompt("Inserire Nome");
+console.log(userName);
+
+const userSurname = prompt("Inserire Cognome");
+console.log(userSurname);
+
+const userColor = prompt("Inserire colore preferito");
+console.log(userColor);
+
+//concateniamo i dati
+let randmPswrd = (`${userName}${userSurname}${userColor}21`)
+console.log(randmPswrd);
+
+//visualizziamo il risultato
+document.getElementById("randmPswrd").innerHTML = randmPswrd;
+
+
+
+
