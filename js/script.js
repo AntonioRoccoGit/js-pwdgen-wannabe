bbbd7de8231e7f74d61362dc6af90ae149f05dc0
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-08 19:57:33 +0100

    added random nuber generator

--- js/script.js
+++ js/script.js
@@ -11,8 +11,10 @@ console.log(userSurname);
 const userColor = prompt("Inserire colore preferito");
 console.log(userColor);
 
+
+let randomNumbr = Math.floor(Math.random() * 100);
 //concateniamo i dati
-let randmPswrd = (`${userName}${userSurname}${userColor}21`)
+let randmPswrd = (`${userName}${userSurname}${userColor}${randomNumbr}`)
 console.log(randmPswrd);
 
 //visualizziamo il risultato
